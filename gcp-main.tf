19a2ab95790d215ca36971417d4918b1ae7e2561
mtossato <72095990+mtossato@users.noreply.github.com>
2022-04-02 12:16:42 -0300

    alterando location

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,12 +1,12 @@
 resource "google_app_engine_application" "app" {
   project     = "hackathon-6aso-grupo-07"
-  location_id = "us-west"
+  location_id = "us-central"
 }
 
 resource "google_artifact_registry_repository" "my-repo" {
   provider = google-beta
 
-  location = "us-west1"
+  location = "us-central1"
   repository_id = "hackathon6asogrupo07"
   description = "Imagens Docker"
   format = "DOCKER"
