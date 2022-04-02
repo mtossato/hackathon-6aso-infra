fecb71a9757121bf0068fac45befcb64743af432
mtossato <72095990+mtossato@users.noreply.github.com>
2022-04-02 13:02:05 -0300

    template

--- gcp-main.tf
+++ gcp-main.tf
@@ -2,6 +2,13 @@ resource "google_cloud_run_service" "default" {
   name     = "cloudGrupo7"
   location = "us-central1"
 
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
 
   traffic {
     percent         = 100
