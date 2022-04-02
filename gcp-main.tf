b4e19600cd06e341189359234736dd862509140f
mtossato <72095990+mtossato@users.noreply.github.com>
2022-04-02 12:36:48 -0300

    voltando

--- gcp-main.tf
+++ gcp-main.tf
@@ -3,7 +3,7 @@ resource "google_app_engine_application" "app_hackathong7" {
   location_id = "us-central"
 }
 
-resource "google_artifact_registry_repository" "my-repog7" {
+resource "google_artifact_registry_repository" "my-repo" {
   provider = google-beta
 
   location = "us-central1"
